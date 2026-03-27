b01f3357832cdbd0c50839179c7a589b5d7f22bc
Chahd tr <ctrabelsi62@gmail.com>
2026-03-27 12:43:29 +0100

    Your commit message describing the changes

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,21 @@ import express from "express";
 
 const app = express();
 
+// Parser le JSON
+app.use(express.json());
+
+// Parser l'URL-encoded (pour x-www-form-urlencoded)
+app.use(express.urlencoded({ extended: true }));
+
+// Test GET
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
+});
+
+// Webhook POST
+app.post('/api/webhook', (req, res) => {
+  console.log('Payload GitHub reçu :', req.body);   
+  res.status(200).send('Webhook received 🚀');
 });
 
 app.listen(5000, () => {
